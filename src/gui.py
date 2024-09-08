b3e7deb5b328f1262ccb41198b84f505515d8f05
catRoman-Laptop <catlinroman84@gmail.com>
2024-09-08 07:04:13 -0700

    updated gui

--- src/gui.py
+++ src/gui.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import ttk, Button, Label, Frame, Toplevel, LEFT, RIGHT, CENTER, scrolledtext
+from tkinter import ttk, Button, Label, Frame, Toplevel, LEFT, RIGHT, CENTER, scrolledtext, PhotoImage
 import sys
 import os
 from main import Linehaul_Booking, cancel_linehaul
@@ -32,7 +32,7 @@ def start_gui():
     icon_path = os.path.join(base_path, 'assets', 'chan_gui', 'chan_img.ico')
 
     root.iconbitmap(icon_path)
-    root.geometry("400x400")
+    root.geometry("400x450")
     root.resizable(False, False)
 
 
@@ -43,6 +43,13 @@ def start_gui():
     style.configure('Text.TLabel', font=("Arial",10))
     style.configure('TButton', font=("Arial",10))
     
+    # Load and display the image
+    image_path = os.path.join(os.path.dirname(__file__),'..', 'assets', 'chan_gui', 'chan_img-3.png')
+    image = PhotoImage(file=image_path)
+    image_label = Label(root, image=image)
+    image_label.image = image  
+    image_label.pack(pady=10)
+    image_label.place(relx=0.5, rely=0.38, anchor=CENTER)
 
     logging.info("authenticating")
     auth_label = ttk.Label(root, text="Authenticating...", style="Title.TLabel")
@@ -51,15 +58,17 @@ def start_gui():
 
     access_token = oauth.get_OAuth_token()
     auth_label.destroy()
+    image_label.destroy()
     #access_token = oauth.get_oauth_token(root)
     logging.info("authenticated")
 
     greeting = ttk.Label(root, text="Welcome The Chan", style="Title.TLabel")
     greeting.pack(pady=20)
 
-    message = ttk.Label(root, text="The chan is here to help \nwith your outbound tasks.", style="Text.TLabel")
-    message.pack(padx=40, pady=20)
-
+    image_label2 = Label(root, image=image)
+    image_label2.image = image 
+    image_label2.pack(pady=40)
+    
     message_2 = ttk.Label(root, text="Automate:", style="SubTitle.TLabel")
     message_2.pack(padx=10, pady=10)
 
@@ -73,6 +82,8 @@ def start_gui():
     print_labels = ttk.Button(root, text="Quit",padding=(10,5), command=lambda: sys.exit(0))
     print_labels.pack(pady=5)
 
+    message = ttk.Label(root, text="The chan is here to help with your outbound tasks.", style="Text.TLabel")
+    message.pack(padx=0, pady=15)
     root.mainloop()
 
 def start_linehaul(gui_button, gui_root):
@@ -98,6 +109,12 @@ def show_confirmation_dialog(root, callback):
 
     sv_ttk.set_theme("dark")
 
+    style =ttk.Style()
+    
+    style.configure('SubTitle.TLabel', font=("Arial",12))
+ 
+    style.configure('TButton', font=("Arial",10))
+
   # Center the dialog on the root window
     root.update_idletasks()
     root_width = root.winfo_width()
@@ -114,11 +131,11 @@ def show_confirmation_dialog(root, callback):
     dialog.geometry(f"{dialog_width}x{dialog_height}+{dialog_x}+{dialog_y}")
 
 
-    label = ttk.Label(dialog, text="Do you want to go full Chan?").pack(padx=20)
-    button_frame = Frame(dialog)
-    button_frame.pack(pady=40)
-    confirm_btn = ttk.Button(button_frame, text="Yes", style="text",command=on_confirm, padding=(10,5)).pack(side=LEFT, padx=20)
-    cancel_btn = ttk.Button(button_frame, text="No",style="text", command=on_cancel, padding=(10,5)).pack(side=RIGHT, padx=20)
+    label = ttk.Label(dialog, text="Do you want to go full Chan?", style="SubTitle.TLabel").pack(padx=20, pady=20)
+    button_frame = ttk.Frame(dialog)
+    button_frame.pack(pady=20)
+    confirm_btn = ttk.Button(button_frame, text="Yes", style="TButton",command=on_confirm, padding=(10,5)).pack(side=LEFT, padx=20)
+    cancel_btn = ttk.Button(button_frame, text="No",style="TButton", command=on_cancel, padding=(10,5)).pack(side=RIGHT, padx=20)
 
     dialog.transient(root)
     dialog.grab_set()
